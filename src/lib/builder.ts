a5df4731a415767abee39ce8cef429c4fb0fe139
Uddeshya Sharma <47824972+Uddeshya08@users.noreply.github.com>
2026-07-15 18:17:24 +0530

    Update build error files

--- src/lib/builder.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-import { builder } from '@builder.io/react';
-
-const key = process.env.NEXT_PUBLIC_BUILDER_API_KEY;
-if (key) builder.init(key);
-
-export { builder };
-export const BUILDER_ENABLED = Boolean(key);
